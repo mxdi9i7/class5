9e28472896da827f3f21f784459b6dd9774b228a
Summer <summer@Summers-MacBook.local>
2016-10-02 19:15:32 -0400

    Asdf

--- news.php
+++ news.php
@@ -2,13 +2,27 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="news.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://use.fontawesome.com/570079994e.js"></script>
 </head>
 <body>
 <?php
     include("include/header.php");
  ?>
- <section style="margin-top: 300px;">
- 	234
+ <section style="margin-top: 130px;">
+ 	<div class="row">
+ 		<div class="col-md-8">
+ 			<img src="">
+ 			<h1>Article title here</h1>
+ 			<p></p>
+ 			<a href="">Button</a>
+ 		</div>
+ 		<div class="col-md-4">
+ 			<input type="" name="">
+ 			<h1>Search here</h1>
+ 		</div>
+ 	</div>
  </section>
  <?php
  	include("include/footer.php");
