a2bb53ccd13f0bfae16fd30545acb75543db0232
mxdi9i7 <peter.zheng88228@gmail.com>
2016-10-02 18:09:48 -0400

    adsf

---
+++ include/header.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/footer.css">
+        <link rel="stylesheet" href="css/index.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://use.fontawesome.com/bda7691915.js"></script>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    </head>
+    <body id="body">
+        <nav class="nav-fixed-top">
+            <img src="http://themearmada.com/demos/sharkfin/img/logo.png" class="navigationLogo">
+            <ul>
+                <li><a class="active" href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li><a href="news.html">News</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li class="signupBtn"><a href="#">Sign up</a></li>
+            </ul>
+        </nav>
