043633e51a8c4550c8fdbbf8bbd91c1c18927ad2
Ling Zhu <Ling Zhu>
2016-10-09 12:00:42 -0400

    asdf

--- portfolio.php
+++ portfolio.php
@@ -1,11 +1,181 @@
-
 <?php
 	include ('include/header.php');
 ?>
-	<section style="margin-top:150px;">
-		123456
-	</section>
+        <link rel="stylesheet" type="text/css" href="css/portfolio.css">
+        <section id="page-title_pfl">
+           <h2 class="title-white_pfl">Our Work</h2>
+        </section>
+        <section id="content_pfl">
+          <div class="container_pfl">
+            <div class="content-title">
+              <ul class="list-inline_pfl">
+                <li><a href="#" data-toggle="portfilter" data-target="all">All</a></li>
+                <li><a href="#" data-toggle="portfilter" data-target="all">Graphics</a></li>
+                <li><a href="#" data-toggle="portfilter" data-target="all">Web</a></li>
+                <li><a href="#" data-toggle="portfilter" data-target="all">Videos</a></li>
+              </ul>
+            </div>
+            <div class="contect-list">
+              <ul class="list-inline portfolio text-center">
+
+              <!--Project Thumbnail-->
+              <li data-tag="graphics">
+                <div class="thumbs">
+                  <img src="image/portfolio1.jpg" class="img-responsive" alt="Image Size 430x280">
+                    <a href="individual-work.html">
+                      <span class="title">
+                        <span><i class="fa fa-link fa-2x"></i></span>
+                      </span> 
+                    </a>
+                </div>
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>Breakroom</h5>
+                </div>
+              </li>
+              
+              <!--Project Thumbnail-->
+              <li data-tag="web">
+                <div class="thumbs">
+                  <img src="image/portfolio2.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>White Sneaker</h5>
+                </div>
+              </li>
+              
+              <!--Project Thumbnail-->
+              <li data-tag="videos">
+                <div class="thumbs">
+                  <img src="image/portfolio3.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>Theme Armada</h5>
+                </div>
+              </li>
+              
+              <!--Project Thumbnail-->
+              <li data-tag="web">
+                <div class="thumbs">
+                  <img src="image/portfolio1.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>Breakroom</h5>
+                </div>
+              </li>
+              
+              <!--Project Thumbnail-->
+              <li data-tag="web">
+                <div class="thumbs">
+                  <img src="image/portfolio2.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>White Sneaker</h5>
+                </div>
+              </li>
+              
+              <!--Project Thumbnail-->
+              <li data-tag="graphics">
+                <div class="thumbs">
+                  <img src="image/portfolio3.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>Theme Armada</h5>
+                </div>
+              </li>
+              
+              <!--Project Thumbnail-->
+              <li data-tag="videos">
+                <div class="thumbs">
+                  <img src="image/portfolio3.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>Theme Armada</h5>
+                </div>
+              </li>
+              
+              
+              <!--Project Thumbnail-->
+              <li data-tag="web">
+                <div class="thumbs">
+                  <img src="image/portfolio2.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>White Sneaker</h5>
+                </div>
+              </li>
+              
+              <!--Project Thumbnail-->
+              <li data-tag="web">
+                <div class="thumbs">
+                  <img src="image/portfolio1.jpg" class="img-responsive" alt="Image Size 430x280">
+                  <a href="individual-work.html">
+                    <span class="title">
+                      <span><i class="fa fa-link fa-2x"></i></span>
+                    </span> 
+                  </a>
+                </div>
+                
+                <div class="project-title_pfl margin-30_pfl">
+                  <h5>Breakroom</h5>
+                </div>
+              </li>
+              </ul>
+          </div>
+        </section>
+        <!--Message-->
+        <section id="message">
+          <div class="container_1">
+            <div>
+              <div class="col-md-pfl text-center">
+                <h4 class="gray-pfl">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h4>
+              </div>
+            </div>
+          </div>
+        </section>
+  </body>
 <?php
 	include ('include/footer.php');
 ?>
-
+</html>
