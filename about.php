1f5553399a9cf8087a03e093f9c5aa0027581214
ge song <ge song>
2016-10-09 14:22:59 -0800

    asdf

--- about.php
+++ about.php
@@ -1,7 +1,58 @@
 <?php
 	include ('include/header.php');
 ?>
+<link rel="stylesheet" type="text/css" href="css/about.css">
+<div class="top">
+		<p>About Us</p>
+		<hr>
+	</div>
+	<div class="container">
+		<div class="pa">
+			<h1>Here Is Why We Are So Amazing!</h1>
+			<h2>Vestibulum non turpis sed diam volutpat scelerisque.</h2>
+			<b><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium diam quis ante
+				<br>
+				 	scelerisque interdum. Praesent non orci nunc. Nulla sit amet felis in nibh ultricies
+				<br> 
+					blandit vel vitae ipsum. Aenean at dolor arcu, a commodo metus. In purus purus,
+				<br>
+					egestas a hendrerit non, euismod ut tortor. Maecenas placerat nunc at eros tincidunt
+				<br>
+					sagittis. Quisque facilisis dapibus ullamcorper. Phasellus ac mauris libero, vel
+				<br>
+				 	placerat felis. Vestibulum non turpis sed diam volutpat scelerisque.
+				<br>
+				<br>
 
+
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium diam quis ante
+				<br>
+				 scelerisque interdum. Praesent non orci nunc. Nulla sit amet felis in nibh ultricies
+				 <br>
+				 blandit vel vitae ipsum.
+			</p></b>
+		</div>
+		<div class="pic">
+			<img src="http://themearmada.com/demos/sharkfin/img/about-img.png">
+		</div>
+	</div>
+	<div class="second">
+		<h1>Meet The Leaders</h1>
+		<center>
+			<img src="http://themearmada.com/demos/sharkfin/img/team-01.png">
+			<img src="http://themearmada.com/demos/sharkfin/img/team-02.png">
+			<img src="http://themearmada.com/demos/sharkfin/img/team-03.png">
+			<img src="http://themearmada.com/demos/sharkfin/img/team-04.png">	
+		</center>	
+	</div>
+	<div class="footer">
+		<center>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+			<br>
+			 eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			</p>
+		</center>
+	</div>
 <?php
 	include ('include/footer.php');
 ?>
