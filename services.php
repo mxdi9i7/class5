9119497fa254ba8300ab25f5f48efae25a004860
李佳童 <Jessielee@lijiatongdeMacBook-Pro.local>
2016-10-09 10:32:52 -0400

    woolala

--- services.php
+++ services.php
@@ -9,7 +9,21 @@
 			<h2>Services</h2>
 		</div>
 	</section>
-	
+	<section class="blueContainer">
+		<div class="container">
+			<div class="servicesTab">
+				<ul>
+	              <li class="active"><a data-toggle="tab" href="http://themearmada.com/demos/sharkfin/services.html#1"><i class="fa fa-rocket fa-4x"></i></a></li>
+	              <li><a data-toggle="tab" href="http://themearmada.com/demos/sharkfin/services.html#2"><i class="fa fa-heart fa-4x"></i></a></li>
+	              <li><a data-toggle="tab" href="http://themearmada.com/demos/sharkfin/services.html#3"><i class="fa fa-truck fa-4x"></i></a></li>
+	              <li><a data-toggle="tab" href="http://themearmada.com/demos/sharkfin/services.html#4"><i class="fa fa-cogs fa-4x"></i></a></li>
+	              <li><a data-toggle="tab" href="http://themearmada.com/demos/sharkfin/services.html#5"><i class="fa fa-lock fa-4x"></i></a></li>
+	              <li><a data-toggle="tab" href="http://themearmada.com/demos/sharkfin/services.html#6"><i class="fa fa-check fa-4x"></i></a></li>
+	            </ul>
+			</div>
+			
+		</div>
+	</section>
 
 <?php
 	include ('include/footer.php');
