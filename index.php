ee6315990192e4680224f28961af490ee65ee9dd
Mingjian Mao <mmao@Mingjians-MacBook-Air.local>
2016-10-08 23:14:52 -0400

    asdf

--- index.php
+++ index.php
@@ -21,27 +21,106 @@
                 <li class="signupBtn"><a href="#">Sign up</a></li>
             </ul>
         </nav>
-    <section class="photoSection">
-        <img src="img/header1" width="300" height="300">
+    <section class="splashPage1">
+    <center class="splashContainer">
+        <div>
+            <i class="fa fa-arrow-circle-o-left" ></i>
+        </div>
+        <div>
+            <img src="image/header1.png" width="1150" height="380">
+            <h1>Welcome to Sharkfin, an incredibly unique web and mobile experience for your users.</h1> 
+        </div>
+        <div class="inputContainer">
+            <button type="button"> LEARN MORE</button> 
+        </div>
+        <div class="iconContainer">
+            <i1 class="fa fa-circle"></i1>
+            <i2 class="fa fa-circle"></i2>
+        </div>
+    </center>
     </section>
-
-
+    <section class="postSection">
+        <div class="row">
+        <div class="col-md-4 infoDiv">
+            <i1 class="fa fa-mobile-phone fa-3x"></i1>
+            <br>
+            <br>
+            <br>
+            <p1>Mobile Responsive</p1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a mauris dui. Nam eu risus neque, sed blandit ante. Integer eget massa.</p>
+        </div>
+        <div class="col-md-4 infoDiv">
+            <i2 class="fa fa-thumbs-up fa-3x"></i2>
+            <br>
+            <br>
+            <br>
+            <p1>Built With Bootstrap</p1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a mauris dui. Nam eu risus neque, sed blandit ante. Integer eget massa.</p>
+        </div>
+        <div class="col-md-4 infoDiv">
+            <i3 class="fa fa-flag fa-3x"></i3>
+            <br>
+            <br>
+            <br>
+            <p1>Over 249 Icons</p1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a mauris dui. Nam eu risus neque, sed blandit ante. Integer eget massa.</p>
+        </div>
+    </section>
+    <section class="logoSection">
+    <center class="logoContainer">
+        <div>
+            <i class="fa fa-rocket fa-4x"></i>
+            <i class="fa fa-heart fa-4x"></i>
+            <i class="fa fa-truck fa-4x"></i>
+            <i class="fa fa-cogs fa-4x"></i>
+            <i class="fa fa-lock fa-4x"></i>
+            <i class="fa fa-check fa-4x"></i> 
+        </div>  
+    </center>
+    <center class="headContainer">
+    <div>
+        <span>Lorem ipsum dolor sit amet, consectetur </span> <span>adipisicing elit.</span>
+    </div>
+    </center>
+    <section class="introSection">
+        <div class="row">
+        <div class="col-md-6 introContainer1">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-6 introContainer2">
+            <p>Lorem ipsum dolor sit amet, eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="introContainer">
+            <button type="button">CONTACT US</button> 
+        </div>
+    </section>
+    <div class="adContainer">
+            <image src="image/clients-01.png" height="100" width="170">
+            <image src="image/clients-02.png" height="100" width="170">
+            <image src="image/clients-03.png" height="100" width="170">
+            <image src="image/clients-04.png" height="100" width="170">
+            <image src="image/clients-01.png" height="100" width="170">
+            <image src="image/clients-04.png" height="100" width="170">
+    </div>
+    <center class="concludeContainer">
+        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</span><span> eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+    </center>
         <footer>
             <div class="footerInfoText">
                 <span>1234 Main Street Atlanta, GA 30305 | </span>
                 <span>405.555.5555 | </span>
                 <span><i class="glyphicon glyphicon-envelope"></i><a href="#"> support@themearmada.com</a></span>
             </div>
-            <div class="footerDivider"></div>
-            <ul>
-                <li><i class="fa fa-twitter fa-3x"></i></li>
-                <li><i class="fa fa-facebook fa-3x"></i></li>
-                <li><i class="fa fa-google fa-3x"></i></li>
-                <li><i class="fa fa-instagram fa-3x"></i></li>
-                <li><i class="fa fa-pinterest fa-3x"></i></li>
-                <li><i class="fa fa-linkedin fa-3x"></i></li>
-                <li><i class="fa fa-github fa-3x"></i></li>
-            </ul>
+            <center class="footerDivider">
+            <br>
+                <i class="fa fa-twitter fa-3x"></i>
+                <i class="fa fa-facebook fa-3x"></i>
+                <i class="fa fa-google fa-3x"></i>
+                <i class="fa fa-instagram fa-3x"></i>
+                <i class="fa fa-pinterest fa-3x"></i>
+                <i class="fa fa-linkedin fa-3x"></i>
+                <i class="fa fa-github fa-3x"></i>
+            </center>
         </footer>
         <div class="bottomContainer">
             <p>&copy;Copyright. All Rights Reserved. Created By <span>Peter Zheng</span></p>
