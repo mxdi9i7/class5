32aa7344de1ba5a7ad6bff54ccf5d6d3dcbf2eff
jun jiang <junjiang@jundeMacBook-Pro.local>
2016-10-09 08:25:13 -0400

    asdf

--- signup.php
+++ signup.php
@@ -8,7 +8,17 @@
 		<h2>Sign Up</h2>
 	</div>
 </section>
-
+<section class="mid">
+	<div class="logoBox">
+		<div class="threeLogo">
+			<ul>
+				<li><a href="https://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+	      		<li><a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+				<li><a href="https://www.google.com/"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+			</ul>
+		</div>
+	</div>
+</section>
 <?php
 	include ('include/footer.php');
 ?>
