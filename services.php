03f4a2b862463850f20cafa1f5bd5330f0c64912
李佳童 <Jessielee@lijiatongdeMacBook-Pro.local>
2016-10-09 10:19:38 -0400

    woolala

--- services.php
+++ services.php
@@ -1,14 +1,15 @@
 <?php
 	include ('include/header.php');
 ?>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/services.css">
 	
 	<section class="pageTitle">
 		<div class="title">
 			<h2>Services</h2>
 		</div>
-		
 	</section>
+	
 
 <?php
 	include ('include/footer.php');
