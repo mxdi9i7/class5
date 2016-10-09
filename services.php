a2b757c08660b36af94737266fc15317b8647881
李佳童 <Jessielee@lijiatongdeMacBook-Pro.local>
2016-10-09 09:56:47 -0400

    woolala

--- services.php
+++ services.php
@@ -1,7 +1,20 @@
 <?php
 	include ('include/header.php');
 ?>
+	<link rel="stylesheet" type="text/css" href="services.css">
+	
+	<section id="page-title">
+	    <div class="container">
+	      <div class="row">
+	        <div class="col-md-12 title-blue">
+	          <h2>Services</h2>
+	        </div>
+	      </div>
+	    </div>
+  	</section>
 
 <?php
 	include ('include/footer.php');
 ?>
+
+ 
